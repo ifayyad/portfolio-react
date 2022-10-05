4338049e2b8d1c4fd84643c7b37b174754b8ac90
ifayyad <fayyadan@gmail.com>
2022-10-05 21:56:47 +0200

    adding a navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,53 @@
-import React from 'react'
+import React, {useState} from 'react';
+import {FaBars, FaTimes} from 'react-icons/fa';
+import Logo from '../assets/logo.png';
 
-function Navbar() {
+const Navbar = () => {
+
+  const [nav, setNav] = useState(false)
+  const handleClick = () => setNav[!nav]
+
   return (
-    
-    <div className='fixed w-full h-[80px]' >
-        Navbar
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'  >
+      <div>
+          <img src={Logo} alt="Logo image" style={{width: '50px'}} />
+      </div>
+
+        {/* Menu */}
+          <ul className='hidden md:flex'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Skills</li>
+            <li>Work</li>
+            <li>Contact</li>
+          </ul>
+       
+
+        {/* Hamburger */}
+        <div onClick={handleClick} className='md:hidden z-10'>
+          {!nav ? <FaBars /> : <FaTimes />}
+        </div>
+
+        {/* Mobile menu */}
+        <ul className={
+          !nav
+            ? 'hidden'
+            : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
+        }>
+            <li className='py-6 text-xl'>Home</li>
+            <li className='py-6 text-xl'>About</li>
+            <li className='py-6 text-xl'>Skills</li>
+            <li className='py-6 text-xl'>Work</li>
+            <li className='py-6 text-xl'>Contact</li>
+          </ul>
+     
+
+
+        {/* Social media icons */}
+        <div>
+
+        </div>
+
     </div>
 
   )
